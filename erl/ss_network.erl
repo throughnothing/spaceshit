df98a9ea2b4e2820049e37f9facc412920ea34fd
Vat Raghavan <vat@crowdtilt.com>
2013-06-23 14:56:12 -0700

    accept keeps returning close, wtf?

--- erl/ss_network.erl
+++ erl/ss_network.erl
@@ -1,49 +1,75 @@
-
 -module(ss_network).
-
-
-
-
 
 -export([
 
     listen_on/3,
-
     start/0,
     stop/1,
 
 %%%%%%%%%%%%%%%%%%%%
 %%%%% internal %%%%%
 %%%%%%%%%%%%%%%%%%%%
-
+    handle_player/2,
     internal_listen_on/2,
     internal_listen_loop/1
 
 ]).
 
-
-
-
-
 internal_listen_on(TheIP, ThePort) ->
+    io:format("listening on socket: ~p~n", [ThePort]),
+    { ok, ListeningSocket } = gen_tcp:listen(ThePort,
+        [ binary, {active, false }, {reuseaddr, true}, {packet,0 } ] ),% {ip, TheIP}]),
+    ListeningSocket
+.
 
-    { ok, ListeningSocket } = gen_tcp:listen(ThePort, [{ip, TheIP}]),
-    ListeningSocket.
-
-
-
-
+fn (ListeningSocket) ->
+    io:format("listen socket: ~p~n", [ListeningSocket]),
+    RetVal = gen_tcp:accept(ListeningSocket),
+    case RetVal of
+        {ok, ServerSocket } ->
+            ServerSocket;
+        {error, Reason} ->
+            io:format("Can't Listen cause Reason = ~p~n", [Reason]),
+            not_ok
+end
+.
 
 internal_listen_loop(ListeningSocket) ->
-
-    { ok, ServerSocket } = gen_tcp:accept(ListeningSocket),
+    %{ ok, ServerSocket } = gen_tcp:accept(ListeningSocket),
+    ServerSocket = fn (ListeningSocket),
     io:format("    + accepted socket at ~w~n", [ServerSocket]),
-    handle_new_server(ServerSocket),
-    internal_listen_loop(ListeningSocket).
+    spawn(fun() -> internal_listen_loop(ServerSocket ) end),
+    spawn( fun() -> handle_player(ListeningSocket, [] ) end)
+.
+    %spawn(fun() -> handle_player(ServerSocket, [] ) end),
+    %internal_listen_loop(ListeningSocket).
 
+handle_player( Socket, PlayerPid ) ->
+    inet:setopts(Socket, [{active,once}]),
+    io:format("Call handle_player~n"),
+    receive
+        {tcp, Socket, Msg } ->
+            io:format("Server received : ~p~n", [Msg]),
+            gen_tcp:send(Socket, Msg ),
+            handle_player(Socket, PlayerPid);
+        {tcp, Socket, <<"join", _/binary>> } ->
+            Pid = handle_new_server(Socket),
+            handle_player( Socket, Pid );
+        {tcp, Socket, <<"quit", _/binary >>} ->
+            io:format("Closed socket"),
+            gen_tcp:close(Socket)
+    end.
+        %{tcp, Socket, "join" } ->
+            %Pid = handle_new_server(Socket),
+            %handle_player( Socket, Pid );
 
+        %%{tcp, Socket, <<"info", _/binary >>} ->
+        %{tcp, Socket, "info" } ->
+            %gen_tcp:send(Socket,  ss_player:get_info(PlayerPid));
 
-
+       %{tcp, Socket, <<"part", _/binary >>} ->
+            %gen_tcp:close(Socket)
+            %.
 
 handle_new_server(ServerSocket) ->
 
@@ -51,17 +77,8 @@ handle_new_server(ServerSocket) ->
     MatchMakerPID = get(matchmaker),
     ss_matchmaker:add_player(MatchMakerPID, PlayerPid).
 
-
-
-
-
 spawn_listen_process(IP, Port) ->
-
     spawn(fun() -> internal_listen_loop(internal_listen_on(IP, Port)) end).
-
-
-
-
 
 network_core_loop() ->
 
@@ -73,13 +90,9 @@ network_core_loop() ->
 
         terminate ->
             io:format("Network core loop exiting~n"),
+            %gen_tcp:close(Socket),
             ok
-
     end.
-
-
-
-
 
 listen_on(ServerPid, IP, Port) ->
 
@@ -87,18 +100,10 @@ listen_on(ServerPid, IP, Port) ->
     ServerPid ! { listen, IP, Port },
     ok.
 
-
-
-
-
 start() ->
 
     io:format("  + ss_network starting~n"),
     spawn(fun() -> network_core_loop() end).
-
-
-
-
 
 stop(WhichServer) ->
 
--- erl/ss_serv.erl
+++ erl/ss_serv.erl
@@ -1,38 +1,22 @@
-
 -module(ss_serv).
 
-
-
-
-
 -export([
-
     default_options/0,
-
     init_server_core_loop/1,
-
     start/0,
-      start/1,
-      
+    start/1,
+    stop/0,
     stop/1
 
 ]).
 
-
-
-
-
 default_options() ->
 
     [ { auto_listen, true             },
-      { port,        8008             }, 
+      { port,        8008             },
       { ip,          {0,0,0,0}        },
       %{ ip,          {10,1,175,209}        },
       { name,        "Default server" } ].
-
-
-
-
 
 init_server_core_loop(Options) ->
 
@@ -46,51 +30,39 @@ init_server_core_loop(Options) ->
     put(network, proplists:get_value(net, Options)),
     put(matchmaker, MatchMakerPid),
     case Auto of
-    
-        true -> 
+
+        true ->
             self() ! {listen, proplists:get_value(ip, Options), proplists:get_value(port, Options)};
-            
+
         false ->
             ok
-    
+
     end,
 
     io:format("  - server core loop beginning~n"),
 
     server_core_loop().
 
-
-
-
-
 server_core_loop() ->
 
     receive
-    
+
         { listen, IP, Port } ->
             Net = get(network),
             io:format("    - server core loop issuing a listen on ~w~n", [Net]),
             ss_network:listen_on(Net,IP,Port),
             server_core_loop();
-    
+
         terminate ->
+            io:format("stopping server!"),
             ok
-    
+
     end.
 
-
-
-
-
 start() ->
-
     start([]).
 
-
-
-
-
-start(Options) -> 
+start(Options) ->
 
     FinalOpts = ss_util:merge_settings(Options, default_options()),
     io:format("Starting with options~n  ~p~n", [FinalOpts]),
@@ -98,16 +70,16 @@ start(Options) ->
     Net = ss_network:start(),
     io:format("  - Net is ~w~n", [Net]),
 
-    spawn(fun() -> init_server_core_loop(FinalOpts ++ [{net, Net}]) end).
+    Pid = spawn(fun() -> init_server_core_loop(FinalOpts ++ [{net, Net}]) end),
+    put(server, Pid)
+.
 
-
-
-
+stop() ->
+    stop(get(server)).
 
 stop(WhichServer) ->
 
     io:format("Stopping server.~n"),
 
-    WhichServer ! terminate,
-    ok.
+    WhichServer ! terminate.
 
