6709cd3ad56d8055c6be7de6fb6181748555e356
Forest Belton <fbelton@theore.tc>
2013-06-23 08:01:07 -0700

    Add state rendering routines

--- js/main.js
+++ js/main.js
@@ -1,26 +1,72 @@
 ;(function() {
     var d = document,
         canvas = d.getElementById('display'),
-        socket = io.connect('http://localhost');
+        socket = io.connect('http://localhost'),
+        objs = {};
 
     if(!canvas.getContext) {
         // error shit
     }
 
+    // Load context & assets
     var ctx = canvas.getContext('2d');
-
-    // Load BG
     var bg = d.getElementById('bg');
-    ctx.drawImage(bg, 0, 0);
+    var sprites = d.getElementsByClassName('sprite');
 
+    // Draw screen
+    var draw = function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(bg, 0, 0);
+        ctx.drawImage(bg, 0, 600);
+
+        for(var id in objs) {
+            var obj = objs[id];
+            drawRotated(sprites[obj.type], obj.x, obj.y, obj.r);
+        }
+    };
+
+    // Draw single object
+    var drawRotated = function(image, x, y, angle) {
+        ctx.save();
+
+        ctx.translate(x, y);
+        ctx.rotate(angle);
+        ctx.drawImage(image, -image.width/2, -image.height/2);
+
+        ctx.restore();
+    };
+ 
+    // Handle middleware communication
     socket.emit('join', {});
     socket.on('frame', function(data) {
         console.log('got frame: ');
         console.log(data);
 
-        if(data.format != 'info' && data.format != 'delete')
-            return;
+        switch(data.format) {
+            case 'info':
+                objs[data.id] = data;
+            break;
 
-        // display shit, yo
+            case 'delete':
+                delete objs[data.id];
+            break;
+        }
     });
+    
+    // requestAnimationFrame shim
+    window.requestAnimFrame = (function() {
+        return window.requestAnimationFrame       ||
+               window.webkitRequestAnimationFrame ||
+               window.mozRequestAnimationFrame    ||
+               function(callback) {
+                   window.setTimeout(callback, 1000 / 60);
+               };
+    })();
+
+    // Try to draw at proper times
+    var renderLoop = function() {
+        requestAnimFrame(renderLoop);
+        draw();
+    };
+    renderLoop();
 })();
